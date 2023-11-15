a36adb8246677a24a59843913fdaffb5502b422f
Sarahi Aguilar Mercado <100244936+sxaguilar@users.noreply.github.com>
2023-11-14 22:25:41 -0600

    add password requirements

--- app.js
+++ app.js
@@ -87,12 +87,62 @@ function createTask() {
     }
 }
 
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+
+            //password verification 
+            var myInput = document.getElementById("psw");
+            var letter = document.getElementById("letter");
+            var capital = document.getElementById("capital");
+            var number = document.getElementById("number");
+            var length = document.getElementById("length");
+          //when password field is selected
+            myInput.onfocus = function() {
+              document.getElementById("message").style.display = "block";
+            }
+          //exits password field
+            myInput.onblur = function() {
+              document.getElementById("message").style.display = "none";
+            }
+          //Typing in the password field
+            myInput.onkeyup = function() {
+              //validate lowercase letters
+              var lowerCaseLetters = /[a-z]/g;
+              if(myInput.value.match(lowerCaseLetters)){
+                  letter.classList.remove("invalid");
+                  letter.classList.add("valid");
+              }
+              else {
+                  letter.classList.remove("valid");
+                  letter.classList.add("invalid");
+              }
+              //validate capital letters
+              var upperCaseLetters = /[A-Z]/g;
+              if(myInput.value.match(upperCaseLetters)){
+                  capital.classList.remove("invalid");
+                  capital.classList.add("valid");
+              }
+              else{
+                  capital.classList.remove("valid");
+                  capital.classList.add("invalid");
+              }
+              //validate numbers
+              var numbers = /[0-9]/g;
+              if(myInput.value.match(numbers)){
+                  number.classList.remove("invalid");
+                  number.classList.add("valid");
+              }
+              else{
+                  number.classList.remove("valid");
+                  number.classList.add("invalid");
+              }
+              //validate length
+              if(myInput.value.length >= 8) {
+                  length.classList.remove("invalid");
+                  length.classList.add("valid");
+              }
+              else{
+                  length.classList.remove("valid");
+                  length.classList.add("invalid");
+              }
+            }
+
+  
